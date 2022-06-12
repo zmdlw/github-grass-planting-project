f1344f9ec7be5aad6518b2106f845a4be1e10228
zmdlw <giraffe0582@gmail.com>
2022-06-12 09:24:13 +0900

    commit

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const simpleGit =  require("simple-git");
 
 const FILE_PATH = "./data.json";
 
-const DATE = moment().format();
-
-const data = {
-    date: DATE
+const makeCommit = (x, y) => {
+    const DATE = moment().subtract(1, 'y').add(1, 'd')
+        .add(x, 'w').add(y, 'd').format();
+    const data = {
+        date: DATE
+    }
+    jsonfile.writeFile(FILE_PATH, data, ()=>{
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }).push();
+    });
 }
 
-jsonfile.writeFile(FILE_PATH, data);
-
-
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE });
+makeCommit(3, 3);
